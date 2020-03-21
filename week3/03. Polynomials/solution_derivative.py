45cfeab0cb143d3e0ff47c0cd5c5a4ee3e7a1e46
Hristiyana <hpmalinova@github.com>
2020-03-22 00:03:18 +0200

    Fix main

--- week3/03. Polynomials/solution_derivative.py
+++ week3/03. Polynomials/solution_derivative.py
@@ -1,15 +1,11 @@
 import sys
 
-from monomial import Monomial
 from polynomial import Polynomial
 
 def main():
-	str_polynomial = sys.argv[1] #str
-	mon = Monomial(-1, 1)
-	print(mon)
-	#polynomial = Polynomial(str_polynomial)
-	#result = polynomial.find_derivative()
-	#print(f'The derivative of f(x) = {str_polynomial} is: {result}')
+	str_polynomial = sys.argv[1]
+	polynomial = Polynomial(str_polynomial)
+	print(polynomial.find_derivative())
 
 if __name__ == '__main__':
 	main()
