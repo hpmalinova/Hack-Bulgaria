51feb2abb3a95086b5676bd5a5830f64c030d9ad
Hristiyana <hpmalinova@github.com>
2020-03-21 19:18:10 +0200

    Fix tests

--- week3/03. Polynomials/test_monomial.py
+++ week3/03. Polynomials/test_monomial.py
@@ -1,60 +1,106 @@
 import unittest
 from monomial import Monomial
 
-class TestMonomial(unittest.TestCase):
-	def test_convert_monomial_of_power_greater_than_1_to_tuple(self):
-		monomial = Monomial('7x^5') # 7x^5
+class TestPrintCoeffMonomial(unittest.TestCase):
+	def test_when_coeff_equals_1_then_dont_print_it(self):
+		monomial = Monomial(1, 5)
 
-		expected_coeff = 7
-		expected_power = 5
+		expected = 'x^5'
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)
+		self.assertEqual(str(monomial), expected)
 
-	def test_convert_monomial_of_power_1_to_tuple(self):
-		monomial = Monomial('7x') # 7x^1
+	def test_when_coeff_equals_negative_1_then_print_only_minus(self):
+		monomial = Monomial(-1, 5)
 
-		expected_coeff = 7
-		expected_power = 1
+		expected = '-x^5'
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)
+		self.assertEqual(str(monomial), expected)
 
-	def test_convert_monomial_of_power_0_to_tuple(self):
-		monomial = Monomial('7') # 7x^0
+	def test_when_coeff_equals_0_then_return_empty_string(self):
+		monomial = Monomial(0, 5)
 
-		expected_coeff = 7
-		expected_power = 0
+		expected = ''
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)	
+		self.assertEqual(str(monomial), expected)
 
-	def test_convert_monomial_of_power_greater_than_1_to_tuple_with_negative_power(self):
-		monomial = Monomial('7x^-5') # 7x^5
+	def test_when_coeff_is_greater_than_one_then_print_it(self):
+		monomial = Monomial(5, 10)
 
-		expected_coeff = 7
-		expected_power = -5
+		expected = '5x^10'
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)
+		self.assertEqual(str(monomial), expected)
 
-	def test_convert_monomial_of_power_1_to_tuple_with_negative_coeff(self):
-		monomial = Monomial('-7x') # 7x^1
+	def test_when_coeff_is_less_than_negative_one_then_print_it(self):
+		monomial = Monomial(-5, 10)
 
-		expected_coeff = -7
-		expected_power = 1
+		expected = '-5x^10'
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)
+		self.assertEqual(str(monomial), expected)
 
-	def test_convert_monomial_of_power_0_to_tuple_with_negative_coeff(self):
-		monomial = Monomial('-7') # 7x^0
+class TestPrintPowerMonomial(unittest.TestCase):
+	def test_when_power_is_zero_then_print_only_coeff(self):
+		monomial = Monomial(5, 0)
 
-		expected_coeff = -7
-		expected_power = 0
+		expected = '5'
 
-		self.assertEqual(monomial.get_coeff(), expected_coeff)	
-		self.assertEqual(monomial.get_power(), expected_power)	
+		self.assertEqual(str(monomial), expected)
+
+	def test_when_power_is_one_then_print_only_x(self):
+		monomial = Monomial(5, 1)
+
+		expected = '5x'
+
+		self.assertEqual(str(monomial), expected)
+
+	def test_when_power_is_greater_than_one_then_print_it(self):
+		monomial = Monomial(5, 5)
+
+		expected = '5x^5'
+
+		self.assertEqual(str(monomial), expected)
+
+	def test_when_power_is_less_than_zero_then_print_it(self):
+		monomial = Monomial(5, -5)
+
+		expected = '5x^-5'
+
+		self.assertEqual(str(monomial), expected)
+
+class TestFindDerivative(unittest.TestCase):
+	def test_when_coeff_equals_zero_then_return_empty_monomial(self):
+		monomial = Monomial(0, 3)
+
+		expected = Monomial(0, 0)
+
+		self.assertEqual(monomial.find_derivative(), expected)		
+
+	def test_when_power_equals_zero_then_return_empty_monomial(self):
+		monomial = Monomial(5, 0)
+
+		expected = Monomial(0, 0)
+
+		self.assertEqual(monomial.find_derivative(), expected)	
+
+	def test_when_power_equals_one_then_return_the_same_coeff_and_power_0(self):
+		monomial = Monomial(5, 1)
+
+		expected = Monomial(5, 0)
+
+		self.assertEqual(monomial.find_derivative(), expected)			
+
+	def test_when_positive_coeff_and_power_then_return_derivative(self):
+		monomial = Monomial(3, 2)
+
+		expected = Monomial(6, 1)
+
+		self.assertEqual(monomial.find_derivative(), expected)	
+
+	def test_when_negative_coeff_and_power_then_return_derivative(self):
+		monomial = Monomial(-3, -2)
+
+		expected = Monomial(6, -3)
+
+		self.assertEqual(monomial.find_derivative(), expected)						
 
 if __name__ == '__main__':
 	unittest.main()	
