210f503edc48d132ed787d38ecc6d58e6bc96f27
Hristiyana <hpmalinova@github.com>
2020-05-19 16:41:02 +0300

    Add db.py and urls table

---
+++ week12/db.py
@@ -0,0 +1,34 @@
+from sqlalchemy import (Column, Integer, String)
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_NAME = 'urls.db'
+
+engine = create_engine(f"sqlite:///{DB_NAME}")
+Base = declarative_base()
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+
+class Url(Base):
+    __tablename__ = "urls"
+    url_id = Column(Integer, primary_key=True)
+    url = Column(String(300), nullable=False, unique=True)
