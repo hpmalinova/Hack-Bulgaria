f09ad02e684fc83dd8b2d164a82101285e6acc0f
Hristiyana <hpmalinova@github.com>
2020-05-19 16:37:21 +0300

    Fix relative urls

--- week12/main.py
+++ week12/main.py
@@ -1,42 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-def print_links_in_site(url):
-    response = requests.get(url)
-    html = response.content.decode("UTF-8")
-    soup = BeautifulSoup(html, 'html.parser')
-    for link in soup.find_all('a'):
-        new_link = link.get('href')
-        if new_link and not new_link.startswith('#'):
-            print(new_link)
+from urllib.parse import urljoin
+# from requests.exceptions import HTTPError
 
 
 # BFS
-def find_all_urls(master_url):
-    queue = [master_url]
-    visited = [master_url]
+def find_all_urls(url):
+    try:
+        response = requests.get(url, timeout=5)
+        url = response.url
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+
+    queue = [url]
+    visited = [url]
+    # Add url to BD
 
     while queue:
         url = queue.pop()
 
-        print('MAIN URL: ', url)
+        try:
+            response = requests.get(url, timeout=5)
+            print('MAIN URL: ', url)  # no
+        except Exception as err:
+            print(f'Other error occurred: {err}')
 
-        response = requests.get(url)
         html = response.content.decode("UTF-8")
         soup = BeautifulSoup(html, 'html.parser')
 
         for link in soup.find_all('a'):
-            new_link = link.get('href')
-            if new_link and new_link not in visited and not new_link.startswith('#'):
-                if new_link.startswith('/'):
-                    new_link = url + new_link[1:]
-                    print('NEW_LINK: ', new_link)
-                    queue.append(new_link)
-                    visited.append(new_link)
+            try:
+                new_link = link.get('href')
+
+                if new_link and not new_link.startswith('#'):
+                    new_link = urljoin(url, new_link)
+                    if new_link not in visited:
+                        if 'link.php?id=' in new_link:
+                            new_link = requests.get(new_link, timeout=5).url
+                        print('NEW_LINK: ', new_link)
+                        visited.append(new_link)
+                        # ADD url to BD
+                        queue.append(new_link)
+            # except HTTPError as http_err:
+            #     print(f'HTTP error occurred: {http_err}')
+            except Exception:
+                pass
 
 
 url = 'https://register.start.bg/'
-# print_links_in_site(url)
-print_links_in_site('http://link.php?id=59518')
-# find_all_urls(url)
+url2 = 'https://www.fmi.uni-sofia.bg/'
+url3 = 'https://about.google/'
+find_all_urls(url)
